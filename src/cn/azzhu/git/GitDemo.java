50ca929023594e987d8734a1854301ed110c3d76
zaz5630 <670049410@qq.com>
2017-03-05 18:11:56 +0800

    github update
    github update

--- src/cn/azzhu/git/GitDemo.java
+++ src/cn/azzhu/git/GitDemo.java
@@ -3,5 +3,6 @@ package cn.azzhu.git;
 public class GitDemo {
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub...");
+		System.out.println("GitHub服务器更新，向本地推送新内容...");
 	}
 }
