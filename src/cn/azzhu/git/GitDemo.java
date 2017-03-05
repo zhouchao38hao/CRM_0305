db686e4b76f544c3724cb3694ca8778576b70c3f
zaz5630 <670049410@qq.com>
2017-03-05 18:21:15 +0800

    github update

--- src/cn/azzhu/git/GitDemo.java
+++ src/cn/azzhu/git/GitDemo.java
@@ -4,5 +4,6 @@ public class GitDemo {
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub...");
 		System.out.println("GitHub服务器更新，向本地推送新内容...");
+		System.out.println("GitHub又更新了，本地仓库知道吗？...");
 	}
 }
